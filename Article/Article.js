178f0392376bde547826e1f6095485e1d30014fd
Laura reed <pilesoflaura@gmail.com>
2020-02-18 17:23:42 -0700

    If run this code with have the class article created

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,8 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+const articleComponent = () => {
+  const article = document.createElement('div');
+  article.classList.add('article');
+  console.log(article);
+}
